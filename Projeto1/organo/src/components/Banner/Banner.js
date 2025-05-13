da0fcb897284a6559ad7bf5599408210aabb3b69
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-05-13 10:06:32 -0300

    feat: adiciona banner da apgina inicial

---
+++ Projeto1/organo/src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img
+                className="img"
+                src="/images/banner.png"
+                alt="Banner pagina principal"
+            />
+        </header>
+    );
+}
+
+export default Banner;
