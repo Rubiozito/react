9834c56657c96e0a8115c575a5ab6e6a23752cd9
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-05-20 15:33:21 -0300

    inicio projeto2

---
+++ projeto2/src/components/Sidebar/index.jsx
@@ -0,0 +1,9 @@
+import Logo from "./assets/Logo.svg";
+
+export default function Sidebar() {
+    return (
+        <aside>
+            <img src={Logo} alt="Logo do CodeConnect" />
+        </aside>
+    );
+}
